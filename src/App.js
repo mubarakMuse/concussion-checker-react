f19c1b0bc56399a63a79a98f3b9ee35ee4080a27
mubarakMuse <mubarak014@gmail.com>
2021-02-25 18:55:32 -0600

    push depoly and changes

--- src/App.js
+++ src/App.js
@@ -11,14 +11,15 @@ import Home from './views/Home'
 class App extends Component {
   render() {
   return ( 
-    <React.Fragment>
-           <Router>
-            <Switch>
-              <Route exact default path= "/" component={Home} />
-              <Route exact path= "/concussion-checker" component={ConcussionChecker} />
-            </Switch>
-          </Router>
-    </React.Fragment>
+    // <React.Fragment>
+    //        <Router>
+    //         <Switch>
+    //           <Route exact default path= "/" component={Home} />
+    //           <Route exact path= "/concussion-checker" component={} />
+    //         </Switch>
+    //       </Router>
+    // </React.Fragment>
+    <ConcussionChecker/>
   );
 };
 }
--- src/data/dataApi.js
+++ src/data/dataApi.js
@@ -1,10 +1,10 @@
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// let  gifUrl = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=M3erJ2EvHLhoSNjZQFOuUa5w9CQDXRhB&limit=5"
+// let  gifUrl = "http://api.giphy.com/v1/gifs/search?q=ryan+gosling"
 
 export const nearByHospitalUrl = (coords) => {
 return (proxyurl + `https://maps.googleapis.com/maps/api/place/nearbysearch/json
 ?location=${coords.latitude},${coords.longitude}
 &radius=1500
 &types=hospital
-&key=AIzaSyAWbscDIvvENp5nKXUrXmq31UcC6QE0mdU`)
+&key=`)
 }
