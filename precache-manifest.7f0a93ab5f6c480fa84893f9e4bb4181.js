8b927fadb539abb14620b6794f31c9bb9beb53e3
mubarakMuse <mubarak014@gmail.com>
2021-02-25 18:32:38 -0600

    Updates

---
+++ precache-manifest.7f0a93ab5f6c480fa84893f9e4bb4181.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b49ff4e33faa5116cb32ebfa836e80",
+    "url": "/concussion-checker-react/index.html"
+  },
+  {
+    "revision": "082446a58a5a3febb377",
+    "url": "/concussion-checker-react/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "ed3d65ed3a2bd35bd140",
+    "url": "/concussion-checker-react/static/css/main.349edf32.chunk.css"
+  },
+  {
+    "revision": "082446a58a5a3febb377",
+    "url": "/concussion-checker-react/static/js/2.b484211b.chunk.js"
+  },
+  {
+    "revision": "4aa0fe1eaee3dcf45338a38641d40f08",
+    "url": "/concussion-checker-react/static/js/2.b484211b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed3d65ed3a2bd35bd140",
+    "url": "/concussion-checker-react/static/js/main.cdf5cb14.chunk.js"
+  },
+  {
+    "revision": "8ec5b0ec1c24b614a10b",
+    "url": "/concussion-checker-react/static/js/runtime-main.c272fa6e.js"
+  },
+  {
+    "revision": "33dbb03ad6793cde7f5ca7bc3f5cb540",
+    "url": "/concussion-checker-react/static/media/CDC.33dbb03a.jpg"
+  },
+  {
+    "revision": "1b683b4ac47de9d3bad653539695e567",
+    "url": "/concussion-checker-react/static/media/ClevelandClinic.1b683b4a.jpeg"
+  },
+  {
+    "revision": "cd4579404be3b5df509463414e82dd1c",
+    "url": "/concussion-checker-react/static/media/ResearchGate.cd457940.png"
+  },
+  {
+    "revision": "f945d9943d717d4c36f65725cec4c289",
+    "url": "/concussion-checker-react/static/media/WebMD_logo.f945d994.png"
+  },
+  {
+    "revision": "c7f30d24e46ff0da23126de93ec26def",
+    "url": "/concussion-checker-react/static/media/logo.c7f30d24.png"
+  }
+]);
